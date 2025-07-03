256a2fb6c390de599bcac55a0fd1f29eda0923f0
이혜빈 <bin020129@gmail.com>
2025-07-03 17:33:29 +0900

    Initial Commit

---
+++ Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by 이혜빈 on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
